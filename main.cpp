1fed676dc0b16d109507b428a59fa6603f3f94f3
laibaabdulhameed <laibaabdulhameed098765@gmail.com>
2026-05-28 09:15:10 +0100

    add payBill,overDue and CalculateBill

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Customer{
     double outstanding_balance;
 
 };
- void addCustomer(Customer customer[], int &customerCount){
+void addCustomer(Customer customer[], int &customerCount){
         string name,address,meter_no,id;
         int used_units;
         cout<<"Enter the Name:"<<"\n";
@@ -33,7 +33,69 @@ struct Customer{
          customerCount++;
             }
 
- void displayCustomer(Customer customer[], int customerCount){
+
+
+void calculateBill(Customer customer[], int &customerCount){
+    double bill;
+    string id;
+    cout<<"Enter the Customer ID:"<<"\n";
+    cin>>id;
+      for(int i=0; i<customerCount; i++){
+         if(customer[i].id==id){
+            int units = customer[i].used_units;
+            cout<<"Used Units:"<<units<<"\n";
+            if(units<=100){
+                bill= units*5;
+            }else if(units<=200){
+                bill=(100*5)+(units-100)*8;
+            }else if(units<=300){
+                 bill=(100*5)+(100*8)+(units-200)*12;
+            }else if(units<=400){
+                 bill=(100*5)+(100*8)+(100*12)+(units-300)*15;
+            }
+            bill = bill+50+100;  //fixed charges
+            bill = bill+(bill*0.17); //GST 17%
+             cout<<"Bill:"<<bill<<" Rs"<<"\n";
+             customer[i].total_bill = bill;
+             customer[i].outstanding_balance = bill;
+         }}
+        
+}
+
+void payBill(Customer customer[], int &customerCount){
+    double bill;
+    string id;
+    cout<<"Enter the Customer ID to pay bill:"<<"\n";
+    cin>>id;
+     for(int i=0; i<customerCount; i++){
+         if(customer[i].id==id){
+            cout<<"Total Bill: "<< customer[i].total_bill<<"\n";
+            cout<<"How much you want to pay?"<<"\n";
+            cin>>bill;
+            if(bill>customer[i].outstanding_balance){
+                cout<<"Amount exceeds balance!"<<"\n";
+            }else{
+            customer[i].amount_paid += bill;
+            customer[i].outstanding_balance -= bill;
+            cout<<"Paid bill:"<< bill<<"\n";
+            cout<<"Remaining Bill Amount:"<< customer[i].outstanding_balance<<"\n";
+            cout<<"Thanks!"<<"\n";
+            }
+         }}
+}
+
+void overDue(Customer customer[], int &customerCount){
+    cout<<"\n === Overdue Customers === \n"
+    for(int i=0; i<customerCount; i++){
+        if(customer[i].outstanding_balance>0){
+            cout<<"ID: "<< customer[i].id<<"\n";
+            cout<<"Name: "<< customer[i].name<<"\n";
+            cout<<"Balance: "<< customer[i].outstanding_balance<<"\n";
+        }
+    }
+}
+
+void displayCustomer(Customer customer[], int customerCount){
         string id;
         cout<<"Enter the Customer ID:"<<"\n";
         cin>>id;
@@ -70,19 +132,18 @@ void showMenu(Customer customer[], int &customerCount)  {
         switch(choice){
             case 1: addCustomer(customer, customerCount); break;
             case 2: displayCustomer(customer, customerCount); break;
-            case 3: // calculateBill later
-            case 4: // payBill later
-            case 5: // viewOverdue later
+            case 3:  calculateBill (customer, customerCount); break;
+            case 4:  payBill  (customer, customerCount); break;
+            case 5:  viewOverdue (customer, customerCount); break;
             case 6: cout << "Goodbye!\n"; return;
             default: cout << "Invalid choice!\n";
         }
     }
-}       
+}  
+
 int main(){
      Customer customer[50];
      int customerCount = 0;
-    //   addCoustomer(customer, customerCount);
-    // displayCustomer(customer, customerCount);
     showMenu(customer, customerCount);
 
 }
